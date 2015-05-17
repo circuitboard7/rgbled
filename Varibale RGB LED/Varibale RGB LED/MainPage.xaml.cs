fbc117b2f2b11fec36f50b7ae8d409b57a17e8b6
circuitboard7 <Daniel.Porrey@hotmail.com>
2015-05-16 19:51:44 -0500

    First pass code. The LED adjustment is working. Still playing with final frequency of PWM.

---
+++ Varibale RGB LED/Varibale RGB LED/MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Windows.Devices.Gpio;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Navigation;
+
+namespace Circuitboard7.RgbLed
+{
+	public sealed partial class MainPage : Page
+	{
+		public static class Constants
+		{
+			public static class Pin
+			{
+				public const int RedLed = 6;
+				public const int GreenLed = 5;
+				public const int BlueLed = 22;
+			}
+
+			public static class Default
+			{
+				public const int LedValue = 50;
+			}
+		}
+
+		private SoftPwm RedPin { get; set; }
+		private SoftPwm GreenPin { get; set; }
+		private SoftPwm BluePin { get; set; }
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+
+			// ***
+			// *** Get a reference to the GPIO Controller
+			// ***
+			var gpio = GpioController.GetDefault();
+
+			// ***
+			// *** Setup the three pins
+			// ***
+			this.RedPin = gpio.OpenPin(Constants.Pin.RedLed, GpioSharingMode.Exclusive).CreateSoftPwm().StartSoftPwm(Constants.Default.LedValue);
+			this.GreenPin = gpio.OpenPin(Constants.Pin.GreenLed, GpioSharingMode.Exclusive).CreateSoftPwm().StartSoftPwm(Constants.Default.LedValue);
+			this.BluePin = gpio.OpenPin(Constants.Pin.BlueLed, GpioSharingMode.Exclusive).CreateSoftPwm().StartSoftPwm(Constants.Default.LedValue);
+
+			// ***
+			// *** Initialize the sliders
+			// ***
+			this.InitializeSliderControl(redSlider, this.RedPin);
+			this.InitializeSliderControl(greenSlider, this.GreenPin);
+			this.InitializeSliderControl(blueSlider, this.BluePin);
+		}
+
+		protected override void OnNavigatedFrom(NavigationEventArgs e)
+		{
+			// ***
+			// *** Stop and Dispose the SoftPwm instances
+			// ***
+			if (this.RedPin != null) { this.RedPin.Dispose(); }
+			if (this.GreenPin != null) { this.GreenPin.Dispose(); }
+			if (this.BluePin != null) { this.BluePin.Dispose(); }
+
+			base.OnNavigatedFrom(e);
+		}
+
+		private void RedSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+		{
+			if (this.RedPin != null)
+			{
+				this.RedPin.Value = (int)e.NewValue;
+			}
+		}
+
+		private void GreenSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+		{
+			if (this.GreenPin != null)
+			{
+				this.GreenPin.Value = (int)e.NewValue;
+			}
+		}
+
+		private void BlueSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+		{
+			if (this.BluePin != null)
+			{
+				this.BluePin.Value = (int)e.NewValue;
+			}
+		}
+
+		private void InitializeSliderControl(Slider slider, SoftPwm pin)
+		{
+			slider.Minimum = pin.MinimumValue;
+			slider.Maximum = pin.MaximumValue;
+			slider.Value = pin.Value;
+		}
+	}
+}
---
+++ Varibale RGB LED/Varibale RGB LED/SoftPwm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using System.Threading;
+
+namespace Circuitboard7.RgbLed
+{
+	public class SoftPwm : IDisposable
+	{
+		private int _counter = 0;
+
+		public SoftPwm(GpioPin pin)
+		{
+			if (pin == null) throw new ArgumentNullException(nameof(pin));
+
+			// ***
+			// *** Set the pin up
+			// ***
+			this.Pin = pin;
+			this.Pin.SetDriveMode(GpioPinDriveMode.Output);
+			this.Pin.Write(GpioPinValue.Low);
+		}
+
+		public GpioPin Pin { get; set; }
+		public int MinimumValue { get; } = 0;
+		public int MaximumValue { get; } = 100;
+
+		private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+		private int _value = 0;
+		private const double _frequencyMultiplier = 3d;
+
+		public async void StartAsync()
+		{
+			this.CheckDisposed();
+
+			while (true)
+			{
+				if (_cancellationTokenSource.IsCancellationRequested)
+				{
+					break;
+				}
+
+				int space = this.MaximumValue - _value;
+
+				if (_value != 0)
+				{
+					this.Pin.Write(GpioPinValue.High);
+				}
+
+				int delayMicroseconds = (int)((double)_value * 100d);
+				await this.DelayMicroSeconds(delayMicroseconds);
+
+				if (space != 0)
+				{
+					this.Pin.Write(GpioPinValue.Low);
+				}
+
+				delayMicroseconds = (int)((double)space * 100d);
+				await this.DelayMicroSeconds(delayMicroseconds);
+			}
+		}
+
+		public Task StopAsync()
+		{
+			this.CheckDisposed();
+			_cancellationTokenSource.Cancel();
+			return Task.FromResult(0);
+		}
+
+		public int Value
+		{
+			get
+			{
+				this.CheckDisposed();
+				return _value;
+			}
+			set
+			{
+				this.CheckDisposed();
+				_value = value;
+				if (_value < this.MinimumValue)
+				{
+					_value = this.MinimumValue;
+				}
+
+				if (_value > this.MaximumValue)
+				{
+					_value = this.MaximumValue;
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			this.CheckDisposed();
+			this.StopAsync();
+			this.Pin.Dispose();
+			this.Pin = null;
+		}
+
+		private void CheckDisposed()
+		{
+			if (this.Pin == null) throw new ObjectDisposedException(nameof(SoftPwm));
+		}
+
+		private async Task DelayMicroSeconds(int delayMicroseconds)
+		{
+			double delayInMiliseconds = (int)((double)delayMicroseconds / 1000d);
+			int multipliedDelay = (int)(delayInMiliseconds / _frequencyMultiplier);
+			await Task.Delay(multipliedDelay, _cancellationTokenSource.Token);
+		}
+	}
+
+	public static class PwmExtensions
+	{
+		public static SoftPwm CreateSoftPwm(this GpioPin pin)
+		{
+			return new SoftPwm(pin);
+		}
+
+		public static SoftPwm StartSoftPwm(this SoftPwm pwm, int initialValue = 0)
+		{
+			pwm.Value = initialValue;
+			pwm.StartAsync();
+			return pwm;
+		}
+	}
+}
