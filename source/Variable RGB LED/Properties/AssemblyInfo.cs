6b94b54dcbb6d121f3036dce72296dec4656f9c4
circuitboard7 <Daniel.Porrey@hotmail.com>
2015-05-18 06:51:30 -0500

    Renamed items

--- source/Variable RGB LED/Properties/AssemblyInfo.cs
+++ source/Variable RGB LED/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Varibale RGB LED")]
+[assembly: AssemblyTitle("Variable RGB LED")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Varibale RGB LED")]
+[assembly: AssemblyCompany("Daniel M. Porrey")]
+[assembly: AssemblyProduct("Variable RGB LED")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
