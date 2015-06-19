1b3aecd3cc24043834f8a15b9b482e91ffde0221
circuitboard7 <Daniel.Porrey@hotmail.com>
2015-06-19 18:33:04 -0500

    Added missing license info to ApplicationSettings.cs

--- source/Variable RGB LED/ApplicationSettings.cs
+++ source/Variable RGB LED/ApplicationSettings.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright © 2015 Daniel Porrey
+//
+// This file is part of Variable RGB LED.
+// 
+// Variable RGB LED is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Variable RGB LED is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Variable RGB LED.  If not, see http://www.gnu.org/licenses/.
+//
+using System;
 using Windows.Storage;
 
 namespace Porrey.RgbLed
